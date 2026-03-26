a505e0b0f0d17a50f99b97e97e8733ea7288e9cf
Martín <martin17@gmail.com>
2026-03-26 10:48:18 -0300

    Add optional `LoggerInterface` dependency to `AbstractHamum` constructor for enhanced logging support

--- src/Server/Hamum/AbstractHamum.php
+++ src/Server/Hamum/AbstractHamum.php
@@ -2,13 +2,14 @@
 
 namespace Tabula17\Satelles\Nexus\Utilis\Server\Hamum;
 
+use Psr\Log\LoggerInterface;
 use Swoole\Server;
 use Tabula17\Satelles\Nexus\Utilis\Server\Hamum\HamumServerInterface;
 use Tabula17\Satelles\Utilis\Config\TCPServerConfig;
 
 abstract class AbstractHamum extends Server implements HamumServerInterface
 {
-    public function __construct(TCPServerConfig $config)
+    public function __construct(TCPServerConfig $config, public ?LoggerInterface $logger = null)
     {
         parent::__construct($config->host, $config->port, $config->mode ?? SWOOLE_BASE, $config->type ?? SWOOLE_SOCK_TCP);
         $sslEnabled = isset($config->ssl) && $config->ssl->enabled;
