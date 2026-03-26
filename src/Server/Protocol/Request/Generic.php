5b28bda661d5e6eb7d0e4c939459948623e22b2f
Martín <martin17@gmail.com>
2026-03-26 13:43:35 -0300

    Introduce `Generic` request handler and update resolver in `Action.php` to use it for improved flexibility.

---
+++ src/Server/Protocol/Request/Generic.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tabula17\Satelles\Nexus\Utilis\Server\Protocol\Request;
+
+use Tabula17\Satelles\Nexus\Utilis\Server\Hamum\HamumServerInterface;
+use Tabula17\Satelles\Nexus\Utilis\Server\Protocol\ProtocolManagerInterface;
+use Tabula17\Satelles\Nexus\Utilis\Server\Protocol\Request\RequestHandlerInterface;
+use Tabula17\Satelles\Nexus\Utilis\Server\Protocol\Status;
+use Tabula17\Satelles\Utilis\Config\AbstractDescriptor;
+
+class Generic extends AbstractDescriptor implements RequestHandlerInterface
+{
+
+    public function handle(int $fd, array $data, HamumServerInterface $server, ?ProtocolManagerInterface $protocolManager): Status
+    {
+        echo 'Generic request handler' . PHP_EOL;
+        echo var_export($data, true) . PHP_EOL;
+        return Status::unknown;
+    }
+}
