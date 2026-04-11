6e7799561b8ee1ec809895a1ef4d16bc6ffdf9f6
Martín <martin17@gmail.com>
2026-04-11 15:46:15 -0300

    Expand `GraphemaHttpErrors` with additional HTTP error codes and messages for enhanced coverage

--- src/Server/Hamum/GraphemaHttpErrors.php
+++ src/Server/Hamum/GraphemaHttpErrors.php
@@ -10,16 +10,33 @@ enum GraphemaHttpErrors
     case BAD_REQUEST;
     case UNAUTHORIZED;
     case FORBIDDEN;
+    case PAYMENT_REQUIRED;
+    case NOT_ACCEPTABLE;
+    case CONFLICT;
+    case GONE;
+    case UNPROCESSABLE_ENTITY;
+    case TOO_MANY_REQUESTS;
+    case BAD_GATEWAY;
+    case SERVICE_UNAVAILABLE;
+    case GATEWAY_TIMEOUT;
     public static function get(int $code): self
     {
         return match ($code) {
             404 => self::NOT_FOUND,
             405 => self::METHOD_NOT_ALLOWED,
-            500 => self::INTERNAL_SERVER_ERROR,
             400 => self::BAD_REQUEST,
             401 => self::UNAUTHORIZED,
             403 => self::FORBIDDEN,
-            default => throw new \Exception('Unknown error code'),
+            402 => self::PAYMENT_REQUIRED,
+            406 => self::NOT_ACCEPTABLE,
+            409 => self::CONFLICT,
+            410 => self::GONE,
+            422 => self::UNPROCESSABLE_ENTITY,
+            429 => self::TOO_MANY_REQUESTS,
+            502 => self::BAD_GATEWAY,
+            503 => self::SERVICE_UNAVAILABLE,
+            504 => self::GATEWAY_TIMEOUT,
+            default => self::INTERNAL_SERVER_ERROR,
         };
     }
     public function httpCode(): int
@@ -31,6 +48,15 @@ enum GraphemaHttpErrors
             self::BAD_REQUEST => 400,
             self::UNAUTHORIZED => 401,
             self::FORBIDDEN => 403,
+            self::PAYMENT_REQUIRED => 402,
+            self::NOT_ACCEPTABLE => 406,
+            self::CONFLICT => 409,
+            self::GONE => 410,
+            self::UNPROCESSABLE_ENTITY => 422,
+            self::TOO_MANY_REQUESTS => 429,
+            self::BAD_GATEWAY => 502,
+            self::SERVICE_UNAVAILABLE => 503,
+            self::GATEWAY_TIMEOUT => 504,
         };
     }
     public function message(): string
@@ -42,6 +68,15 @@ enum GraphemaHttpErrors
             self::BAD_REQUEST => 'Bad Request',
             self::UNAUTHORIZED => 'Unauthorized',
             self::FORBIDDEN => 'Forbidden',
+            self::PAYMENT_REQUIRED => 'Payment Required',
+            self::NOT_ACCEPTABLE => 'Not Acceptable',
+            self::CONFLICT => 'Conflict',
+            self::GONE => 'Gone',
+            self::UNPROCESSABLE_ENTITY => 'Unprocessable Entity',
+            self::TOO_MANY_REQUESTS => 'Too Many Requests',
+            self::BAD_GATEWAY => 'Bad Gateway',
+            self::SERVICE_UNAVAILABLE => 'Service Unavailable',
+            self::GATEWAY_TIMEOUT => 'Gateway Timeout',
         };
     }
     public function json(): array
